b16b4231c75f37664a0835f9c71fa0adaa5980d3
HajCoco92 <77750196+HajCoco92@users.noreply.github.com>
2021-04-26 19:46:10 +0200

    Update autoclient.sql

--- client/autoclient.sql
+++ client/autoclient.sql
@@ -1,31 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : Dim 18 avr. 2021 à 19:55
--- Version du serveur :  5.7.31
--- Version de PHP : 7.3.21
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+Drop database if exists forum;
+Create database forum;
+Use forum;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `autoclient`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `contactinfo`
---
 
 DROP TABLE IF EXISTS `contactinfo`;
 CREATE TABLE IF NOT EXISTS `contactinfo` (
